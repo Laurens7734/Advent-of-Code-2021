545820d6132365020716729c29afbc77a0f89fed
laurens meijerink <laurensmeijerink@hotmail.com>
2021-12-01 10:47:39 +0100

    finished day 1

--- Advent of Code 2021/Day.cs
+++ Advent of Code 2021/Day.cs
@@ -23,4 +23,23 @@ namespace Advent_of_Code_2021
         public abstract string Assignment1();
         public abstract string Assignment2();
     }
+    /*Default day for copy/paste reasons
+     * 
+    public class Day01: Day
+    {
+        public Day01() : base("01")
+        {
+        }
+
+        public override string Assignment1()
+        {
+            return "";
+        }
+
+        public override string Assignment2()
+        {
+            return "";
+        }
+    }
+    */
 }
--- Advent of Code 2021/Days/Day01.cs
+++ Advent of Code 2021/Days/Day01.cs
@@ -6,18 +6,43 @@ namespace Advent_of_Code_2021.Days
 {
     public class Day01: Day
     {
+        private List<int> nums;
         public Day01() : base("01")
         {
+            nums = new List<int>();
+            foreach (string s in input)
+                nums.Add(int.Parse(s));
         }
 
         public override string Assignment1()
         {
-            return "";
+            int increaseCount = 0;
+
+            for(int i =1; i < nums.Count; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                    increaseCount++;
+            }
+            return increaseCount.ToString();
         }
 
         public override string Assignment2()
         {
-            return "";
+            int increaseCount = 0;
+            List<int> windowSum = new List<int>();
+
+            for (int i = 2; i < nums.Count; i++)
+            {
+                windowSum.Add(nums[i] + nums[i - 1] + nums[i - 2]);
+            }
+
+            for (int i = 1; i < windowSum.Count; i++)
+            {
+                if (windowSum[i] > windowSum[i - 1])
+                    increaseCount++;
+            }
+
+            return increaseCount.ToString();
         }
     }
 }

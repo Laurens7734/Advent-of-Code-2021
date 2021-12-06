b59195fca8da40ee29434a46a0ce2e965e627507
laurens meijerink <laurensmeijerink@hotmail.com>
2021-12-06 11:32:25 +0100

    day6

---
+++ Advent of Code 2021/Days/Day06.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_of_Code_2021.Days
+{
+    public class Day06 : Day
+    {
+        int[] startingPosition;
+        public Day06() : base("06")
+        {
+            startingPosition = new int[7];
+            string[] nums = input[0].Split(',');
+            foreach(string s in nums)
+            {
+                startingPosition[int.Parse(s)]++;
+            }
+        }
+
+        public override string Assignment1()
+        {
+            int[] fish = new int[9];
+            for (int i = 0; i < startingPosition.Length; i++)
+            {
+                fish[i] = startingPosition[i];
+            }
+            for (int i = 0; i < 80; i++)
+            {
+                int newFish = fish[0];
+                for (int j = 1; j < fish.Length; j++)
+                {
+                    fish[j - 1] = fish[j];
+                }
+                fish[6] += newFish;
+                fish[8] = newFish;
+            }
+            long sum = 0;
+            foreach (int i in fish)
+                sum += i;
+            return sum.ToString();
+        }
+
+        public override string Assignment2()
+        {
+            long[] fish = new long[9];
+            for (int i = 0; i < startingPosition.Length; i++)
+            {
+                fish[i] = startingPosition[i];
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                long newFish = fish[0];
+                for (int j = 1; j < fish.Length; j++)
+                {
+                    fish[j - 1] = fish[j];
+                }
+                fish[6] += newFish;
+                fish[8] = newFish;
+            }
+            long sum = 0;
+            foreach (long i in fish)
+                sum += i;
+            return sum.ToString();
+        }
+    }
+}
--- Advent of Code 2021/Program.cs
+++ Advent of Code 2021/Program.cs
@@ -7,7 +7,7 @@ namespace Advent_of_Code_2021
     {
         static void Main(string[] args)
         {
-            Day d = new Day05();
+            Day d = new Day06();
             Console.WriteLine(d.Assignment1());
             Console.WriteLine(d.Assignment2());
         }

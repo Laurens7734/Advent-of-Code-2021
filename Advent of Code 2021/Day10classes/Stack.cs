0d4720e66b1da53223df4225bca29634a41cfafa
laurens meijerink <laurensmeijerink@hotmail.com>
2021-12-10 10:38:54 +0100

    day10

---
+++ Advent of Code 2021/Day10classes/Stack.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_of_Code_2021.Day10classes
+{
+    public class Stack
+    {
+        List<char> unresolved;
+
+        public Stack()
+        {
+            unresolved = new List<char>();
+        }
+
+        public bool Next(char c)
+        {
+            switch (c)
+            {
+                case '(':
+                case '[':
+                case '{':
+                case '<': unresolved.Add(c); return true;
+                case ')':
+                case ']':
+                case '}':
+                case '>': return Resolve(c);
+            }
+            return false;
+        }
+
+        public long GetCompletionScore()
+        {
+            long score = 0;
+            for(int i = 1; i <= unresolved.Count; i++)
+            {
+                score *= 5;
+                score += FindScore(unresolved[^i]);
+            }
+            return score;
+        }
+
+        private bool Resolve(char c)
+        {
+            if (unresolved.Count == 0)
+                return false;
+
+            if(FindMatch(unresolved[^1]) == c)
+            {
+                unresolved.RemoveAt(unresolved.Count-1);
+                return true;
+            }
+
+            return false;
+
+        }
+
+        private char FindMatch(char c)
+        {
+            switch (c)
+            {
+                case '(': return ')';
+                case '[': return ']';
+                case '{': return '}';
+                case '<': return '>';
+            }
+            return 'e';
+        }
+
+        private int FindScore(char c)
+        {
+            switch (c)
+            {
+                case '(': return 1;
+                case '[': return 2;
+                case '{': return 3;
+                case '<': return 4;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Advent of Code 2021/Days/Day10.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Advent_of_Code_2021.Day10classes;
+
+namespace Advent_of_Code_2021.Days
+{
+    public class Day10 : Day
+    {
+        public Day10() : base("10")
+        {
+        }
+
+        public override string Assignment1()
+        {
+            long totalScore = 0;
+            foreach(string s in input)
+            {
+                Stack st = new Stack();
+                foreach(char c in s)
+                {
+                    if (!st.Next(c))
+                    {
+                        totalScore += GetScore(c);
+                        break;
+                    }
+                }
+            }
+            return totalScore.ToString();
+        }
+
+        public override string Assignment2()
+        {
+            List<long> scores = new List<long>();
+            foreach (string s in input)
+            {
+                Stack st = new Stack();
+                bool valid = true;
+                foreach (char c in s)
+                {
+                    if (!st.Next(c))
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid)
+                {
+                    scores.Add(st.GetCompletionScore());
+                }
+            }
+
+            scores.Sort();
+            return scores[scores.Count/2].ToString();
+        }
+
+        private int GetScore(char c)
+        {
+            switch (c)
+            {
+                case ')': return 3;
+                case ']': return 57;
+                case '}': return 1197;
+                case '>': return 25137;
+            }
+            return 0;
+        }
+    }
+}

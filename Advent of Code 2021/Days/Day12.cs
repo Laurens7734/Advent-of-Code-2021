b8d94aaf6fd964aed547dc08effe3d30bef6bdde
Laurens <laurensmeijerink@gmail.com>
2021-12-11 14:11:26 +0100

    quick test

---
+++ Advent of Code 2021/Days/Day12.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_of_Code_2021.Days
+{
+    public class Day12 : Day
+    {
+        public Day12() : base("12")
+        {
+        }
+
+        public override string Assignment1()
+        {
+            return "";
+        }
+
+        public override string Assignment2()
+        {
+            return "";
+        }
+    }
+}
